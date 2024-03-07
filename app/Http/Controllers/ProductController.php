793d4298262a6065a28ff86736b44668b7956ea1
Ubaldo Santos <u.santospaton@gmail.com>
2024-03-07 13:47:41 +0100

    Agregada la colección de productos en el controlador de productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 use App\Models\Product;
+use App\Http\Resources\ProductCollection;
 
 class ProductController extends Controller
 {
@@ -13,7 +14,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $product = Product::paginate();
+        return new ProductCollection($product);
+
     }
 
     /**
